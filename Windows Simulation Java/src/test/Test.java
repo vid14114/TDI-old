f9e77c47651270abc1a40f89acbcf576492c3973
Abideen Bamgbala <bam14053@spengergasse.at>
2012-10-11 20:25:55 +0200

    Now able to get a pointer to the handler of the desktop icons. What we can do with it, is still a question which needs to be answered

--- Windows Simulation Java/src/test/Test.java
+++ Windows Simulation Java/src/test/Test.java
@@ -6,6 +6,7 @@ import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.*;
 import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinUser.POINT;
 import com.sun.jna.platform.win32.WinUser.WNDENUMPROC;
 import com.sun.jna.win32.StdCallLibrary;
 
@@ -18,20 +19,29 @@ public class Test{
 	public interface User32 extends StdCallLibrary {
 	      User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
 	      boolean EnumWindows(WinUser.WNDENUMPROC lpEnumFunc, Pointer arg);
-	      int GetWindowTextA(HWND hWnd, byte[] lpString, int nMaxCount);
+	      int GetWindowTextA(HWND hWnd, byte[] lpString, int nMaxCount);	 
+	      boolean SetForegroundWindow(WinDef.HWND hWnd);
+	      boolean CloseWindow(WinDef.HWND hWnd);
+	      WinDef.HWND GetWindow(WinDef.HWND hWnd,
+                  WinDef.DWORD uCmd);
 	   }
 
 	   public static void main(String[] args) {
-	      final User32 user32 = User32.INSTANCE;
+	      final User32 user32 = User32.INSTANCE;	     
 	      user32.EnumWindows(new WNDENUMPROC() {
 	         int count = 0;
 	         
 	         public boolean callback(HWND hWnd, Pointer arg1) {
 	            byte[] windowText = new byte[512];
-	            user32.GetWindowTextA(hWnd, windowText, 512);
+	            user32.GetWindowTextA(hWnd, windowText, 1000);
 	            String wText = Native.toString(windowText);
 	            System.out.println("Found window with text " + hWnd + ", total " + ++count
 	                  + " Text: " + wText);
+	            if(wText.contains("Program Manager")){
+	            	WinDef.HWND h = user32.GetWindow(hWnd, new WinDef.DWORD());
+	            	Pointer n = h.getPointer();
+	            	n.getString(0);
+	            }
 	            return true;
 	         }
 	      }, null);
