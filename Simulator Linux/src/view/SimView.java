fac92050fe7961fe7b32951d9807f23d991fbbab
Abideen Bamgbala <bam14053@spengergasse.at>
2012-12-08 15:52:46 +0100

    Still doing changes, please don't edit

--- Simulator Linux/src/view/SimView.java
+++ Simulator Linux/src/view/SimView.java
@@ -1,16 +1,27 @@
 package view;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.ImageObserver;
 import java.io.*;
+import java.text.AttributedCharacterIterator;
 import java.util.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.TransferHandler;
 import javax.swing.border.LineBorder;
 import control.Configuration;
 import control.DndHandler;
+import control.DragDropListener;
 import model.Icon;
 
 public class SimView extends JFrame{
@@ -22,34 +33,33 @@ public class SimView extends JFrame{
 	 * A randomly generated serialVersion
 	 */
 	private static final long serialVersionUID = 639166567792984188L;
-	int cols=5; //maximum cols
-	int rows=8; //maximum rows
+	int cols=5; //maximum cols, this values should be found out with a formular which is going to be added later on
+	int rows=8; //maximum rows this values should be found out with a formular which is going to be added later on
+	JLabel[][] labels = new JLabel[rows][cols];
 	
 	public SimView()
 	{
 		setTitle("Simulation Linux");
 		setLocation(0, 0);
-		setSize(MAXIMIZED_HORIZ, MAXIMIZED_VERT);
+		setSize(600, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(true);
 		setVisible(true);			
 	}
 
-	public GridLayout initDesk()
-	{		
-		setLayout(new GridLayout(rows, cols));
-		set
-		for(int i=0; i<rows; i++)
+	public void initDesk()
+	{
+		CustomPanel jpanel = new CustomPanel(img, rows, cols);
+		for(int i = 0; i < rows; i++)
 		{
-			for(int j=0; j<cols; j++)
+			for(int j = 0; j < cols; j++)
 			{
-				grid[i][j]=new JLabel();
-				grid[i][j].setBorder(new LineBorder(Color.BLACK));
-				//set the Drag&Drop handler
-				grid[i][j].setTransferHandler(new DndHandler("text", this));				
-				grid[i][j].addMouseListener(this);
-				grid[i][j].setName("");
-				add(grid[i][j]);
+				labels[i][j] = new JLabel();
+				labels[i][j].setBorder(new LineBorder(Color.BLACK));
+				//set the Drag&Drop handler				
+				labels[i][j].addMouseListener(new DragDropListener());
+				labels[i][j].setName("");
+				add(labels[i][j]);
 			}
 		}
 		for(int i=0; i<vs.size(); i++)
@@ -103,5 +113,28 @@ public class SimView extends JFrame{
 		bw.close();
 		//refresh the desktop manager
 		Runtime.getRuntime().exec("xfdesktop --reload").waitFor();
+	}
+}
+
+/**
+ * Had to create a custom JPanel class which extends JPanel to be able to override the paintComponent
+ * and use an image as the background image
+ * @author TDI Team
+ *
+ */
+class CustomPanel extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8731589894224036259L;
+	Image img;
+	
+	public CustomPanel(Image img, int rows, int cols){
+		setLayout(new GridLayout(rows, cols));
+		this.img = img;
+	}
+	
+	public void paintComponent(Graphics g){
+		g.drawImage(img,0,0,null);
 	}
 }
