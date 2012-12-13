bd48ff659afdca12ca7296760b06e125d7c8c8c2
Viktor Vidovic <vid14114@spengergasse.at>
2012-12-13 18:36:33 +0100

    Revert "Fixed a backgroundimage bug :)"
    This reverts commit 7b76474f40adb14fc2302ce7936d4b3fb16cf622.

---
+++ Simulator Linux/src/control/Main.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package control;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+import view.SimView;
+
+/**
+ * @author TDI Team
+ *
+ */
+public class Main {
+	
+	public void run(){
+		try {
+			Configuration config = new Configuration();			
+			SimView view = new SimView(config.getResolution());
+			view.initDesk(config.getIcons(), config.getBackground(), config);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Couldn't load the configuration file for initialization.\n"
+					+" The program can not start without having read the configuaration file. Exiting.");
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.run();
+	}
+
+}
