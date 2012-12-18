ef2acfac53ed9af14a85e1038f28fedc95b6f0bf
Anil Guelcehre <gue14115@spengergasse.at>
2012-12-18 12:44:41 +0100

    I added some stuff bro.

---
+++ TUIO-Simulation/src/control/Launch.java
@@ -0,0 +1,15 @@
+package control;
+
+import view.Desk;
+
+public class Launch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desk d = new Desk();
+	}
+
+}
