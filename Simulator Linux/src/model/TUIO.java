b881956a83c65c8619165858cf8a59e96f6d02b1
Viktor Vidovic <vid14114@spengergasse.at>
2013-02-27 16:05:15 +0100

    TUIO class with getters and setters

--- Simulator Linux/src/model/TUIO.java
+++ Simulator Linux/src/model/TUIO.java
@@ -2,12 +2,34 @@ package model;
 
 public class TUIO {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int id;
+	private int[] pos={0,0};
+	private int minimalRotation;
+	private int rotationPosition;
+	
+	public int getId() {
+		return id;
 	}
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int[] getPos() {
+		return pos;
+	}
+	public void setPos(int posX, int posY) {
+		pos[0]=posX;
+		pos[1]=posY;
+	}
+	public int getMinimalRotation() {
+		return minimalRotation;
+	}
+	public void setMinimalRotation(int minimalRotation) {
+		this.minimalRotation = minimalRotation;
+	}
+	public int getRotationPosition() {
+		return rotationPosition;
+	}
+	public void setRotationPosition(int rotationPosition) {
+		this.rotationPosition = rotationPosition;
+	}
 }
