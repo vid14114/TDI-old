cff20d33539c96c0f956f31c61d765224a9ee426
Anil Guelcehre <gue14115@spengergasse.at>
2013-03-06 11:51:18 +0100

    Added the sockets without the help of Abi even though help was requested.

--- TUIO-Simulation/src/control/Main.java
+++ TUIO-Simulation/src/control/Main.java
@@ -1,5 +1,11 @@
 package control;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 
 import model.TUIO;
@@ -16,12 +22,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Desk d = new Desk();
+		Thread t = new Thread(new SocketListener());
+		t.start();
 		// adding new TUIOs this will be moved away from this later
 		// on - just test values for now
 		// id , x, y, rotation
 		tuios=new HashMap<Integer, TUIO>();
 	}
 	
-	
+
+
 
 }
---
+++ TUIO-Simulation/src/control/SocketListener.java
@@ -0,0 +1,47 @@
+package control;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketListener implements Runnable{
+	public Socket socket;
+	public PrintWriter out;
+	public ObjectInputStream incoming;
+	public SocketListener(){
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			//Create socket connection
+			   try{
+			     socket = new Socket("10.0.0.1", 2345);
+			     //Not used yet.
+			     out = new PrintWriter(socket.getOutputStream(),true);
+			     incoming= new ObjectInputStream(socket.getInputStream());
+			     while(socket.isConnected()){
+			    	 String[] message = ((String)incoming.readObject()).split(";");
+			    	 //Here we get the commands form the server.... 
+			    	 switch(message[0].toLowerCase()){
+			    	 }
+			     }
+			   } catch (UnknownHostException e) {
+			     System.out.println("Unknown host: kq6py");
+			     System.exit(1);
+			   } catch  (IOException e) {
+			     System.out.println("No I/O");
+			     System.exit(1);
+			   } catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
